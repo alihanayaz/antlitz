89fce518335d0fa2b8e21ab340d5e1f95ba48bc0
alihanayaz <alihan@alihanayaz.com>
2026-07-15 02:41:59 +0300

    build: compile dist with react compiler via babel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,13 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
+import reactCompiler from "eslint-plugin-react-compiler";
 
 export default tseslint.config(
   { ignores: ["dist/", "playground/dist/"] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    plugins: { "react-compiler": reactCompiler },
+    rules: { "react-compiler/react-compiler": "error" },
+  },
 );
