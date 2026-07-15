ad57271353a74b1c01dbcde8fc9ff9059f13269d
alihanayaz <alihan@alihanayaz.com>
2026-07-15 22:17:37 +0300

    chore: add playground for visual qa

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import tseslint from "typescript-eslint";
 import reactCompiler from "eslint-plugin-react-compiler";
 
 export default tseslint.config(
-  { ignores: ["dist/"] },
+  { ignores: ["dist/", "playground/dist/"] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
